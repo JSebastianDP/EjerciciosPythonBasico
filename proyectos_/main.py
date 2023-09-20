461d7e207076039ae08b7f86e6e050a7be2e7d1a
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-09-20 11:48:52 -0500

    Se completa el ejercicio de envio de mensajes a whatsapp

--- proyectos_/main.py
+++ proyectos_/main.py
@@ -1,3 +1,5 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+573216692365", "prueba de envio de mensajes", 11, 34)
+#Se deben enviar estos 4 parametros los cuales son el numero de telefono, mensaje hora y segundos adicionalmente, si quieremos que despues de cierre el tab automaticamente debemos agregar true y los segundos que se desean
+pywhatkit.sendwhatmsg("+573216692365", "prueba de envio de mensajes", 11, 34, True, 2)
+
